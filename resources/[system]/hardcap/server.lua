61166c593ece28e60557cfe1cad8ccdde1240dc7
Greve <greve@plazarollespill.no>
2025-01-26 19:16:31 +0100

    Added Globalstate to Playercount

--- resources/[system]/hardcap/server.lua
+++ resources/[system]/hardcap/server.lua
@@ -4,28 +4,30 @@ local list = {}
 RegisterServerEvent('hardcap:playerActivated')
 
 AddEventHandler('hardcap:playerActivated', function()
-  if not list[source] then
-    playerCount = playerCount + 1
-    list[source] = true
-  end
+    if not list[source] then
+        list[source] = true
+        playerCount = playerCount + 1
+        GlobalState.playerCount = playerCount
+    end
 end)
 
 AddEventHandler('playerDropped', function()
-  if list[source] then
-    playerCount = playerCount - 1
-    list[source] = nil
-  end
+    if list[source] then
+        list[source] = nil
+        playerCount = playerCount - 1
+        GlobalState.playerCount = playerCount
+    end
 end)
 
 AddEventHandler('playerConnecting', function(name, setReason)
-  local cv = GetConvarInt('sv_maxclients', 32)
+    local cv = GetConvarInt('sv_maxclients', 32) -- Max player count from server config
 
-  print('Connecting: ' .. name .. '^7')
+    print('Connecting: ' .. name .. '^7')
 
-  if playerCount >= cv then
-    print('Full. :(')
+    if playerCount >= cv then
+        print('Server is full.')
 
-    setReason('This server is full (past ' .. tostring(cv) .. ' players).')
-    CancelEvent()
-  end
+        setReason('This server is full (maximum ' .. tostring(cv) .. ' players).')
+        CancelEvent() -- Cancel the player's connection
+    end
 end)
